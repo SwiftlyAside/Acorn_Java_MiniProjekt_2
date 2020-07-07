8c7bb16089546487c33028d052fa5a848798786e
jun <tpwns107@gmail.com>
2020-07-07 17:13:21 +0900

    Add DB Manager Modify UsersDAO

--- src/DAO/UsersDAO.java
+++ src/DAO/UsersDAO.java
@@ -1,5 +1,13 @@
 package DAO;
 
+import DBCP.DBManager;
+import DTO.UsersDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class UsersDAO {
@@ -8,28 +16,51 @@ public class UsersDAO {
 
     // 1. login
     // 2. id check
-    public Map<String, String> getUserId(String userId) {
-        // Map<String, String> map = new HashMap<>();
-        // Connection conn = XXX.getConnection ***add later***
-        // PreparedStatement pstmt = conn.prepareStatement(getUserIdSQL);
-        // pstmt.setString(1, userId);
-        // ResultSet rs = pstmt.executeQuery();
-        // if (rs.next) { map.put("id", rs.getString(1)); map.put("pass", rs.getString(2)); }
-        // finally { connection close }
-        // return map;
+    public Map<String, String> getUserId(String userId) throws SQLException {
+        Map<String, String> map = new HashMap<>();
+        Connection conn = DBManager.getConnection();
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            pstmt = conn.prepareStatement(getUserIdSQL);
+            pstmt.setString(1, userId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                map.put("id", rs.getString(1));
+                map.put("pass", rs.getString(2));
+                return map;
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBManager.close(conn, pstmt, rs);
+        }
+
+
         return null;
     }
 
     // 1. join member
     public boolean insertUserInfo(UsersDTO userInfo) {
-        // Connection conn = XXX.getConnection ***add later***
-        // PreparedStatement pstmt = conn.prepareStatement(insertUserInfoSQL);
-        // pstmt.setString(1, userInfo.userId);
-        // pstmt.setString(2, userInfo.userPass);
-        // pstmt.setString(3, userInfo.userName);
-        // pstmt.setString(4, userInfo.userEmail);
-        // pstmt.execute();
+        Connection conn = DBManager.getConnection();
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = conn.prepareStatement(insertUserInfoSQL);
 
+            pstmt.setString(1, userInfo.getUserId());
+            pstmt.setString(2, userInfo.getUserPass());
+            pstmt.setString(3, userInfo.getUserName());
+            pstmt.setString(4, userInfo.getUserEmail());
+            pstmt.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBManager.close(conn, pstmt);
+        }
         return true;
     }
 
